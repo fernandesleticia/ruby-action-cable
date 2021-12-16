b57e62e1e2cec578cdc389f66cc7be8d1206d0a0
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-12-15 21:11:15 -0300

    misc.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
+    before_action :logged_in_redirect, only: [:new, :create]
+    
     def new
     end
 
     def create
-        user = User.find_by(username: params[:session][:username])
-        if user && user.authenticate(params[:session][:password])
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id 
             flash[:success] = 'You have successfully logged in'
             redirect_to root_path
@@ -18,5 +20,14 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         flash[:success] = 'You have successufully logged out'
         redirect_to login_path
+    end
+
+    private
+
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = 'You are already logged in'
+            redirect_to root_path
+        end
     end
 end
