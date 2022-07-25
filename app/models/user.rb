6b1177a20539457c272edea7d009919f089c0a9f
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-07-25 16:08:16 -0300

    use soft tabe

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 3, maximum: 15 }, 
-              uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { minimum: 3, maximum: 15 }, 
+            uniqueness: { case_sensitive: false }
     
-    has_secure_password
+  has_secure_password
 
-    has_many :messages
+  has_many :messages
 end
