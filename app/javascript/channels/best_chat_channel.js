dc89104f9c2f05696982ffd827e68b02d9429917
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-07-27 14:56:47 -0300

    add action for when receiving message

--- app/javascript/channels/best_chat_channel.js
+++ app/javascript/channels/best_chat_channel.js
@@ -1,3 +1,22 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "BestChatChannel", room: "Best Room" });
+consumer.subscriptions.create({ channel: "BestChatChannel", room: "Best Room" }, {
+  received(data) {
+    this.appendMessage(data)
+  },
+
+  appendMessage(data) {
+    const html = this.createMessage(data)
+    const element = document.querySelector("[data-best-chat='Best Room']")
+    element.insertAdjacentHTML("beforeend", html)
+  },
+
+  createMessage(data) {
+    return `
+      <article class="chat-line">
+        <span class="speaker">${data["sent_by"]}</span>
+        <span class="body">${data["body"]}</span>
+      </article>
+    `
+  }
+});
