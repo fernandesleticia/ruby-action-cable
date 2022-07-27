b0102c955384091526683e59e67ea3a1e8265214
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-07-27 14:55:23 -0300

    broadcast best chat channel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
+      ActionCable.server.broadcast("best_chat_channel", { body: message.body })
       redirect_to root_path
     end
   end
