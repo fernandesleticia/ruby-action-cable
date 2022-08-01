b3cf28fac83ba1a77f7ba2053cf309be38a71f35
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-08-01 20:03:42 -0300

    update root index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'chat_room#index'
+  root 'best_chat#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
