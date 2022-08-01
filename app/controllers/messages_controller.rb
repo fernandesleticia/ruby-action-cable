5c083d8eac978240ac666a0223936fbdef68b45c
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-08-01 20:03:13 -0300

    add sent_by to broadcast

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,13 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      ActionCable.server.broadcast("best_chat_channel", { body: message.body })
+      ActionCable.server.broadcast(
+        "best_chat_channel", 
+        { 
+          sent_by: 'user name'
+          body: message.body 
+        }
+      )
       redirect_to root_path
     end
   end
